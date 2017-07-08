4baf7f9521230723b2502d46e2ffa785b7124a9e
Codrea Tudor <tudor.codrea@gmail.com>
2017-07-08 09:45:54 +0300

    Update

--- app/src/main/java/com/example/android/booklistingapp_newversion/BookListAdapter.java
+++ app/src/main/java/com/example/android/booklistingapp_newversion/BookListAdapter.java
@@ -1,6 +1,9 @@
 package com.example.android.booklistingapp_newversion;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,9 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 import static android.text.TextUtils.join;
@@ -33,7 +39,24 @@ public class BookListAdapter extends ArrayAdapter<Book> {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.book_item, parent, false);
         }
 
-        Book currentBook = getItem(position);
+        final Book currentBook = getItem(position);
+
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                URL url = null;
+                try {
+                    url = new URL(currentBook.getBookImage());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Bitmap bookImage = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
         //Set title for the book
         TextView titleView = (TextView) listItemView.findViewById(R.id.book_title);
